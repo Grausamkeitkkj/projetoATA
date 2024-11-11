951f7a7ac0b06de875757582cac42db9d9dd6216
Diogo Schultz <diogorschultz2@gmail.com>
2024-11-11 19:49:06 -0300

    adicionando bootstrap

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+require_once("./class/class_aluno_experimental.php");
 require_once("funcoes_uteis.php");
 require_once("form_experimental.php");
 $alunoExperimental = new alunoExperimental();
@@ -10,45 +11,69 @@ $result = $alunoExperimental->getExperimental();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./node_modules/bootstrap/dist/css/bootstrap.css">
     <link rel="stylesheet" href="./css/style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="./node_modules/jquery/dist/jquery.js"></script>
     <title>Document</title>
 </head>
 <body>
+    <div class="main-content">
     <?php include 'menu.php'; ?>
-    <h1 class="titulo-experimental">Próximas Aulas Experimentais</h1>
-    <div class="container-table">
-        <table class="table-experimental">
-            <thead>
-                <tr>
-                    <th>Nome</th>
-                    <th>Idade</th>
-                    <th>Nível</th>
-                    <th>Data</th>
-                    <th>Telefone</th>
-                </tr>
-            </thead>
-            <tbody id="table-body">
-                <?php
-                while ($row = $result->fetch_assoc()) {
-                    if($row['nivel'] == 1){
-                        $row['nivel'] = 'Tiger';
-                    } elseif($row['nivel'] == 2){
-                        $row['nivel'] = 'Adolescente';
-                    } else {
-                        $row['nivel'] = 'Adulto';
-                    }
-                    echo "<tr>";
-                    echo "<td>" . $row['nome'] . "</td>";
-                    echo "<td>" . $row['idade'] . "</td>";
-                    echo "<td>" . $row['nivel'] . "</td>";
-                    echo "<td>" . date('d/m/Y', strtotime($row['data'])) . "</td>";
-                    echo "<td>" . formata_telefone($row['telefone']) . "</td>";
-                    echo "</tr>";
-                }
-                ?>
-            </tbody>
-        </table>
+    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 layout-spacing container-fluid">
+        <div class="box-shadow">
+            <div class="row">
+                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
+                    <h1 class="titulo">Próximas Aulas Experimentais</h1>
+                    <table class="table table-striped table-bordered">
+                        <thead class="table-header-blue">
+                            <tr>
+                                <th>Nome</th>
+                                <th>Idade</th>
+                                <th>Nível</th>
+                                <th>Data</th>
+                                <th>Telefone</th>
+                            </tr>
+                        </thead>
+                        <tbody id="table-body">
+                            <?php
+                            while ($row = $result->fetch_assoc()) {
+                                if ($row['nivel'] == 1) {
+                                    $row['nivel'] = 'Tiger';
+                                } elseif ($row['nivel'] == 2) {
+                                    $row['nivel'] = 'Adolescente';
+                                } else {
+                                    $row['nivel'] = 'Adulto';
+                                }
+                                echo "<tr>";
+                                echo "<td>" . $row['nome'] . "</td>";
+                                echo "<td>" . $row['idade'] . "</td>";
+                                echo "<td>" . $row['nivel'] . "</td>";
+                                echo "<td>" . date('d/m/Y', strtotime($row['data'])) . "</td>";
+                                echo "<td>" . formata_telefone($row['telefone']) . "</td>";
+                                echo "</tr>";
+                            }
+                            ?>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
+                    <h1 class="titulo">Aulas do dia</h1>
+                    <table class="table table-striped table-bordered">
+                        <thead class="table-header-blue">
+                            <tr>
+                                <th>Horário</th>
+                                <th>Nível</th>
+                                <th>Coluna 3</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <!-- Adicione aqui as linhas da nova tabela -->
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
     </div>
+</div>
 </body>
 </html>
--- menu.php
+++ menu.php
@@ -1,20 +1,22 @@
-<ul class="menu">
-    <li><a href="index.php">Home</a></li>
-    <li class="submenu">
-        <a href="#">Cadastro</a>
-        <ul class="submenu-items">
-            <li><a href="experimental.php">Experimental</a></li>
-            <li><a href="#">Aluno</a></li>
-            <li><a href="#">Professor</a></li>
-            <li><a href="#">Responsável</a></li>
-        </ul>
-    </li>
-    <li class="submenu">
-        <a href="#">Serviços</a>
-        <ul class="submenu-items">
-            <li><a href="#">Consultoria</a></li>
-            <li><a href="#">Desenvolvimento</a></li>
-        </ul>
-    </li>
-    <li><a href="#">Contato</a></li>
-</ul>
+<div class="container-menu">
+    <ul class="menu">
+        <li><a href="index.php">Home</a></li>
+        <li class="submenu">
+            <a href="#">Cadastro</a>
+            <ul class="submenu-items">
+                <li><a href="experimental.php">Experimental</a></li>
+                <li><a href="#">Aluno</a></li>
+                <li><a href="#">Professor</a></li>
+                <li><a href="#">Responsável</a></li>
+            </ul>
+        </li>
+        <li class="submenu">
+            <a href="#">Serviços</a>
+            <ul class="submenu-items">
+                <li><a href="#">Consultoria</a></li>
+                <li><a href="#">Desenvolvimento</a></li>
+            </ul>
+        </li>
+        <li><a href="#">Contato</a></li>
+    </ul>
+</div>
